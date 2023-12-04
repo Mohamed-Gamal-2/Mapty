2dd9af9a9d6310b2676d72720e522439674bb24d
Mohamed-Gamal-2 <141522329+Mohamed-Gamal-2@users.noreply.github.com>
2023-12-04 15:40:31 +0200

    adding projects main files + using geolocation

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  location => {
+    const { latitude, longitude } = location.coords;
+    console.log(latitude);
+    console.log(longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  },
+  () => {
+    alert('Can not get your exact location');
+  }
+);
